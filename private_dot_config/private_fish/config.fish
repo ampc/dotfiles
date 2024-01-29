6b8bf367f602ae11fa7c7afb8598c26382bfbddc
António Carvalho <antonio.carvalho@ctw.bmwgroup.com>
2024-01-29 10:11:18 +0000

    add fish config

---
+++ private_dot_config/private_fish/config.fish
@@ -0,0 +1,7 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+  starship init fish | source
+end
+
+test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
+
