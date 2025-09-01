6b3c8463902b0af4731bab0bc9aeb108c1fefafb
Antonio Carvalho <antonio.carvalho@ctw.bmwgroup.com>
2025-09-01 13:34:10 +0100

    update

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -4,4 +4,22 @@ if status is-interactive
 end
 
 set fish_greeting ""
-test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
+set -q KREW_ROOT; and set -gx PATH $PATH $KREW_ROOT/.krew/bin; or set -gx PATH $PATH $HOME/.krew/bin
+
+#if test -z "$ZED_TERM"
+#  test -e "{$HOME}/.iterm2_shell_integration.fish" ; and source "{$HOME}/.iterm2_shell_integration.fish" or true
+#end
+
+
+
+# Created by `pipx` on 2024-08-05 12:28:26
+set PATH $PATH /Users/ctw00844/.local/bin
+
+# Added by OrbStack: command-line tools and integration
+# This won't be added again if you remove it.
+source ~/.orbstack/shell/init2.fish 2>/dev/null || :
+
+# Added by LM Studio CLI (lms)
+set -gx PATH $PATH /Users/ctw00844/.lmstudio/bin
+
+string match -r "kcl" "$PATH" > /dev/null; or set -gx PATH "/usr/local/bin" $PATH
